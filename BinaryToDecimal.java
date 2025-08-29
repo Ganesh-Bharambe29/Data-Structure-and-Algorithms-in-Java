7771451caad1176b53778180dceee52aa502278d
Ganesh Bharambe <ganeshvbharambe@gmail.com>
2025-08-29 19:18:02 +0530

    Add files via upload

---
+++ BinaryToDecimal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class BinaryToDecimal {
+    public static void binToDec(int binNum)
+    {
+        int myNum=binNum;
+        int pow=0;
+        int decNum=0;
+
+        while(binNum>0)
+        {
+            int lastdigit=binNum%10;
+            decNum=decNum+(lastdigit*(int)Math.pow(2,pow));
+            pow++;
+            binNum=binNum/10;
+        }
+        System.out.println("Decimal of the Number"+myNum+":"+decNum);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("The Number is");
+        int n=sc.nextInt();
+        binToDec(n);
+
+    }
+}
