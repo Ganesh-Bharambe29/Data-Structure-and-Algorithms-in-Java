91eb63ed51ee8177cac73507c34a2c346ef9e7d4
Ganesh Bharambe <ganeshvbharambe@gmail.com>
2025-09-13 21:37:53 +0530

    Add files via upload

---
+++ HalfPyramid.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class HalfPyramid {
+
+    public static void floydstraingle(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            int count=1;
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(count+"");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n=sc.nextInt();
+        floydstraingle(n);
+    }
+}
+
+
